9861a9cc7f774e2d72a618e7e9a0bb7322e37b38
JAROT OFC <108053350+JarotOffc@users.noreply.github.com>
2025-05-07 23:51:42 +0700

    New fitur _autoresetlimit.js

---
+++ plugins/_autoresetlimit.js
@@ -0,0 +1,120 @@
+const _0x153561 = _0x3bf9;
+function _0x3bf9(_0x3f9dcc, _0x1364a2) {
+    const _0x2436f3 = _0x2436();
+    return _0x3bf9 = function (_0x3bf9bb, _0xceae66) {
+        _0x3bf9bb = _0x3bf9bb - 0xb0;
+        let _0x4c7c63 = _0x2436f3[_0x3bf9bb];
+        return _0x4c7c63;
+    }, _0x3bf9(_0x3f9dcc, _0x1364a2);
+}
+function _0x2436() {
+    const _0x4b1943 = [
+        'text',
+        'khzwd',
+        'user',
+        '291470wLvVVd',
+        '5256911CwWlYr',
+        'chOrW',
+        '715320ymjyAE',
+        '77774FruLgG',
+        'jid',
+        '1220OLeSNF',
+        '4525LkenxU',
+        '\x20Users\x0a\x0a>\x20*Notes:*\x20Limit\x20di\x20reset\x20setiap\x20hari',
+        '@whiskeysockets/baileys',
+        '\x20/\x20Users\x0a*\x20*Total\x20Reset:*\x20',
+        'default',
+        '\x0a*\x20*Bot\x20Number:*\x20',
+        'settings',
+        'entries',
+        'data',
+        'plaintext',
+        '24zAVoxB',
+        '\x0a*\x20*Reset\x20Status:*\x20Sukses\x0a*\x20*Reset\x20Limit:*\x20',
+        'length',
+        'resetlimitDB',
+        'getName',
+        'encode',
+        'query',
+        'message',
+        'split',
+        '54775PPhbsk',
+        '174AoDNdk',
+        'keys',
+        'map',
+        '36qUETpn',
+        'xKjFx',
+        '332280RzfOxd',
+        '120363320674792053@newsletter',
+        'Message',
+        '`[\x20Reset\x20Limit\x20Notification\x20]`\x0a\x0a*\x20*Bot\x20Name:*\x20'
+    ];
+    _0x2436 = function () {
+        return _0x4b1943;
+    };
+    return _0x2436();
+}
+(function (_0x4a6c9a, _0x1471cb) {
+    const _0x43b89f = _0x3bf9, _0x28efd6 = _0x4a6c9a();
+    while (!![]) {
+        try {
+            const _0xefe094 = -parseInt(_0x43b89f(0xc1)) / 0x1 + parseInt(_0x43b89f(0xc5)) / 0x2 * (-parseInt(_0x43b89f(0xd2)) / 0x3) + -parseInt(_0x43b89f(0xc7)) / 0x4 * (-parseInt(_0x43b89f(0xc8)) / 0x5) + -parseInt(_0x43b89f(0xb5)) / 0x6 * (parseInt(_0x43b89f(0xb4)) / 0x7) + -parseInt(_0x43b89f(0xba)) / 0x8 * (-parseInt(_0x43b89f(0xb8)) / 0x9) + parseInt(_0x43b89f(0xc4)) / 0xa + parseInt(_0x43b89f(0xc2)) / 0xb;
+            if (_0xefe094 === _0x1471cb)
+                break;
+            else
+                _0x28efd6['push'](_0x28efd6['shift']());
+        } catch (_0xc29c9) {
+            _0x28efd6['push'](_0x28efd6['shift']());
+        }
+    }
+}(_0x2436, 0x2793b));
+const {
+    default: _makeWaSocket,
+    makeWALegacySocket,
+    proto,
+    downloadContentFromMessage,
+    jidDecode,
+    areJidsSameUser,
+    generateForwardMessageContent,
+    generateWAMessageFromContent,
+    WAMessageStubType,
+    extractMessageContent,
+    prepareWAMessageMedia
+} = (await import(_0x153561(0xca)))[_0x153561(0xcc)];
+export async function all(_0x152e39) {
+    const _0x273962 = _0x153561, _0xa5e846 = {
+            'xKjFx': function (_0x28b183, _0x3df2fc) {
+                return _0x28b183 - _0x3df2fc;
+            },
+            'chOrW': function (_0xb81d2f, _0x2e630d) {
+                return _0xb81d2f * _0x2e630d;
+            },
+            'khzwd': _0x273962(0xd1),
+            'OpqER': _0x273962(0xb2),
+            'Ncghv': _0x273962(0xbb),
+            'zjErl': function (_0x2e7f7a, _0x25cb7a) {
+                return _0x2e7f7a * _0x25cb7a;
+            }
+        };
+    let _0x36923e = global['db'][_0x273962(0xd0)][_0x273962(0xce)][this[_0x273962(0xc0)][_0x273962(0xc6)]];
+    if (_0x36923e['resetlimit']) {
+        if (_0xa5e846[_0x273962(0xb9)](_0xa5e846[_0x273962(0xc3)](new Date(), 0x1), _0x36923e[_0x273962(0xd5)]) > 0x5265c00) {
+            let _0x4e3b4b = Object[_0x273962(0xcf)](global['db']['data']['users']), _0x4ed3b2 = 0x32;
+            _0x4e3b4b[_0x273962(0xb7)](([_0x16066e, _0x115fda], _0x108041) => Number(_0x115fda['limit'] = _0x4ed3b2));
+            const _0x190ff7 = { 'conversation': _0x273962(0xbd) + this[_0x273962(0xd6)](this[_0x273962(0xc0)][_0x273962(0xc6)]) + _0x273962(0xcd) + this[_0x273962(0xc0)][_0x273962(0xc6)][_0x273962(0xb3)]('@')[0x0] + _0x273962(0xd3) + _0x4ed3b2 + _0x273962(0xcb) + Object[_0x273962(0xb6)](db['data']['users'])[_0x273962(0xd4)] + _0x273962(0xc9) }, _0x5d0379 = proto[_0x273962(0xbc)][_0x273962(0xb0)](_0x190ff7)['finish'](), _0x10f05c = {
+                    'tag': _0xa5e846[_0x273962(0xbf)],
+                    'attrs': {},
+                    'content': _0x5d0379
+                }, _0x331638 = {
+                    'tag': _0xa5e846['OpqER'],
+                    'attrs': {
+                        'to': _0xa5e846['Ncghv'],
+                        'type': _0x273962(0xbe)
+                    },
+                    'content': [_0x10f05c]
+                };
+            conn[_0x273962(0xb1)](_0x331638), _0x36923e[_0x273962(0xd5)] = _0xa5e846['zjErl'](new Date(), 0x1);
+        }
+    }
+    return !0x0;
+}
