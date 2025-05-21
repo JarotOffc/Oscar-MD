c1eb6e7cc92851c90c48f9f55572f78f13ddf291
JAROT OFC <108053350+JarotOffc@users.noreply.github.com>
2025-05-22 01:47:44 +0700

    Neww

---
+++ plugins/exp-ceksn.js
@@ -0,0 +1,129 @@
+// demi kebaikan bersama
+
+const _0x12e04e = _0xa0c0;
+(function (_0x41739c, _0x1708f0) {
+    const _0x4df1c7 = _0xa0c0, _0x340804 = _0x41739c();
+    while (!![]) {
+        try {
+            const _0x445784 = parseInt(_0x4df1c7(0x208)) / 0x1 * (parseInt(_0x4df1c7(0x1f1)) / 0x2) + parseInt(_0x4df1c7(0x20f)) / 0x3 * (parseInt(_0x4df1c7(0x1fc)) / 0x4) + -parseInt(_0x4df1c7(0x1f8)) / 0x5 + parseInt(_0x4df1c7(0x1fa)) / 0x6 + -parseInt(_0x4df1c7(0x1f0)) / 0x7 * (-parseInt(_0x4df1c7(0x202)) / 0x8) + parseInt(_0x4df1c7(0x1f7)) / 0x9 * (parseInt(_0x4df1c7(0x20c)) / 0xa) + -parseInt(_0x4df1c7(0x1fe)) / 0xb;
+            if (_0x445784 === _0x1708f0)
+                break;
+            else
+                _0x340804['push'](_0x340804['shift']());
+        } catch (_0x2f7c34) {
+            _0x340804['push'](_0x340804['shift']());
+        }
+    }
+}(_0x492b, 0xc768e));
+function _0xa0c0(_0x4e354f, _0x372a5a) {
+    const _0x492bbc = _0x492b();
+    return _0xa0c0 = function (_0xa0c049, _0x263a0c) {
+        _0xa0c049 = _0xa0c049 - 0x1ef;
+        let _0x596ed2 = _0x492bbc[_0xa0c049];
+        return _0x596ed2;
+    }, _0xa0c0(_0x4e354f, _0x372a5a);
+}
+const {
+    default: _makeWaSocket,
+    makeWALegacySocket,
+    proto,
+    downloadContentFromMessage,
+    jidDecode,
+    areJidsSameUser,
+    generateForwardMessageContent,
+    generateWAMessageFromContent,
+    WAMessageStubType,
+    extractMessageContent,
+    prepareWAMessageMedia
+} = (await import('@whiskeysockets/baileys'))['default'];
+import { createHash } from 'crypto';
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i, handler = async function (_0x4f64b8, {
+        conn: _0x49f511,
+        text: _0x44dac5,
+        usedPrefix: _0x16524d
+    }) {
+        const _0x432bf5 = _0xa0c0, _0xadfd0f = {
+                'SYqJj': function (_0x43c55b, _0x2b190a) {
+                    return _0x43c55b(_0x2b190a);
+                },
+                'QtYtZ': _0x432bf5(0x1f5),
+                'iRJQK': 'https://qu.ax/XenZo.jpeg',
+                'RkyTy': _0x432bf5(0x1ef),
+                'SVoEW': _0x432bf5(0x20d),
+                'Grhxg': _0x432bf5(0x1f9)
+            };
+        let _0x4966b2 = _0xadfd0f['SYqJj'](createHash, _0x432bf5(0x1f2))[_0x432bf5(0x201)](_0x4f64b8[_0x432bf5(0x205)])['digest'](_0xadfd0f[_0x432bf5(0x20a)]), _0x102a80 = generateWAMessageFromContent(_0x4f64b8['chat'], {
+                'viewOnceMessage': {
+                    'message': {
+                        'messageContextInfo': {
+                            'deviceListMetadata': {},
+                            'deviceListMetadataVersion': 0x2
+                        },
+                        'interactiveMessage': proto[_0x432bf5(0x20e)][_0x432bf5(0x1f6)][_0x432bf5(0x203)]({
+                            'body': proto[_0x432bf5(0x20e)]['InteractiveMessage']['Body'][_0x432bf5(0x203)]({ 'text': _0x432bf5(0x206) }),
+                            'footer': proto[_0x432bf5(0x20e)]['InteractiveMessage']['Footer'][_0x432bf5(0x203)]({ 'text': wm }),
+                            'header': proto[_0x432bf5(0x20e)]['InteractiveMessage'][_0x432bf5(0x207)][_0x432bf5(0x203)]({
+                                'hasMediaAttachment': ![],
+                                ...await prepareWAMessageMedia({ 'image': { 'url': _0xadfd0f[_0x432bf5(0x20b)] } }, { 'upload': _0x49f511[_0x432bf5(0x200)] })
+                            }),
+                            'nativeFlowMessage': proto[_0x432bf5(0x20e)][_0x432bf5(0x1f6)]['NativeFlowMessage']['create']({
+                                'buttons': [
+                                    {
+                                        'name': _0xadfd0f['RkyTy'],
+                                        'buttonParamsJson': '{\x22display_text\x22:\x22copy\x22,\x22id\x22:\x22123456789\x22,\x22copy_code\x22:\x22' + _0x4966b2 + '\x22}'
+                                    },
+                                    {
+                                        'name': _0xadfd0f[_0x432bf5(0x1f3)],
+                                        'buttonParamsJson': _0xadfd0f[_0x432bf5(0x209)]
+                                    }
+                                ]
+                            })
+                        })
+                    }
+                }
+            }, { 'quoted': _0x4f64b8 });
+        await _0x49f511[_0x432bf5(0x204)](_0x4f64b8['chat'], _0x102a80[_0x432bf5(0x1fd)], { 'messageId': _0x4f64b8['key']['id'] });
+    };
+handler[_0x12e04e(0x1f4)] = ['ceksn'], handler['tags'] = ['xp'], handler[_0x12e04e(0x1ff)] = /^(ceksn)$/i, handler[_0x12e04e(0x1fb)] = !![];
+export default handler;
+function _0x492b() {
+    const _0x40d223 = [
+        '9394902ckoBEC',
+        'register',
+        '8mYeGnJ',
+        'message',
+        '13948286EhzBOh',
+        'command',
+        'waUploadToServer',
+        'update',
+        '56bPEKBF',
+        'create',
+        'relayMessage',
+        'sender',
+        '📮\x20SN:\x20Click\x20Tombol\x20Di\x20Bawah',
+        'Header',
+        '1622nXpyGS',
+        'Grhxg',
+        'QtYtZ',
+        'iRJQK',
+        '2319530fSiiiv',
+        'quick_reply',
+        'Message',
+        '147018wgKwty',
+        'cta_copy',
+        '5299DooDFs',
+        '42YyJSUI',
+        'md5',
+        'SVoEW',
+        'help',
+        'hex',
+        'InteractiveMessage',
+        '63WZZDky',
+        '6210265AeKOoc',
+        '{\x22display_text\x22:\x22Owner\x22,\x22id\x22:\x22.owner\x22}'
+    ];
+    _0x492b = function () {
+        return _0x40d223;
+    };
+    return _0x492b();
+}
